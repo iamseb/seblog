305468dfd7be416d0eb43669bca7f497384f9328
Sebastian Potter <iamseb@gmail.com>
2016-07-12 16:00:10 +0100

    add missing files for tests

---
+++ test/support/guardian_test_helper.ex
@@ -0,0 +1,56 @@
+defmodule Seblog.TestGuardianSerializer do
+  @moduledoc false
+
+  alias Seblog.Admin
+
+  @behaviour Guardian.Serializer
+  def for_token(%{error: :unknown}), do: {:error, "Unknown resource type"}
+
+  def for_token(admin = %Admin{}), do: {:ok, "Admin:#{admin.id}"}
+  def from_token("Admin:" <> _id), do: {:ok, %Admin{:email => "test@example.com"}}
+end
+
+defmodule Seblog.GuardianTestHelper do
+  @moduledoc false
+
+  @default_opts [
+    store: :cookie,
+    key: "foobar",
+    encryption_salt: "encrypted cookie salt",
+    signing_salt: "signing salt"
+  ]
+
+  @secret String.duplicate("abcdef0123456789", 8)
+  @signing_opts Plug.Session.init(Keyword.put(@default_opts, :encrypt, false))
+
+  def conn_with_fetched_session(the_conn) do
+    the_conn.secret_key_base
+    |> put_in(@secret)
+    |> Plug.Session.call(@signing_opts)
+    |> Plug.Conn.fetch_session
+  end
+
+  @doc """
+  Helper for running a plug.
+
+  Calls the plug module's `init/1` function with
+  no arguments and passes the results to `call/2`
+  as the second argument.
+  """
+  def run_plug(conn, plug_module) do
+    opts = apply(plug_module, :init, [])
+    apply(plug_module, :call, [conn, opts])
+  end
+
+  @doc """
+  Helper for running a plug.
+
+  Calls the plug module's `init/1` function with
+  the value of `plug_opts` and passes the results to
+  `call/2` as the second argument.
+  """
+  def run_plug(conn, plug_module, plug_opts) do
+    opts = apply(plug_module, :init, [plug_opts])
+    apply(plug_module, :call, [conn, opts])
+  end
+end
