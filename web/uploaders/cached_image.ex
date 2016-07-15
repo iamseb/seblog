a7af30c9a88050f3427f5c44b601936ca58702ad
Sebastian Potter <iamseb@gmail.com>
2016-07-15 13:43:51 +0100

    tweak filenames for uploaded images and thumbs

--- web/uploaders/cached_image.ex
+++ web/uploaders/cached_image.ex
@@ -13,9 +13,10 @@ defmodule Seblog.CachedImage do
   #   end
   # end
 
-  # def filename(version,  {file, _scope}) do
-  #   "#{version}-#{file.file_name}"
-  # end
+  def filename(version,  {file, _scope}) do
+    file_name = "#{version}-#{file.file_name}"
+    Path.basename(file_name, Path.extname(file_name))
+  end
 
   # To add a thumbnail version:
   @versions [:original, :thumb]
@@ -39,81 +40,81 @@ defmodule Seblog.CachedImage do
   def storage_dir(_version, {_file, _scope}) do
     base = Application.get_env(:seblog, Seblog.CachedImage)[:cache_base]
     "#{base}"
-end
+  end
 
-  # Provide a default URL if there hasn't been a file uploaded
-  # def default_url(version, scope) do
-  #   "/images/avatars/default_#{version}.png"
-  # end
+    # Provide a default URL if there hasn't been a file uploaded
+    # def default_url(version, scope) do
+    #   "/images/avatars/default_#{version}.png"
+    # end
 
-  # Specify custom headers for s3 objects
-  # Available options are [:cache_control, :content_disposition,
-  #    :content_encoding, :content_length, :content_type,
-  #    :expect, :expires, :storage_class, :website_redirect_location]
-  #
+    # Specify custom headers for s3 objects
+    # Available options are [:cache_control, :content_disposition,
+    #    :content_encoding, :content_length, :content_type,
+    #    :expect, :expires, :storage_class, :website_redirect_location]
+    #
   def s3_object_headers(_version, {file, _scope}) do
-    [content_type: Plug.MIME.path(file.file_name)]
-end
+      [content_type: Plug.MIME.path(file.file_name)]
+  end
 
 
-def cache_remote_image(url) do
-    IO.puts "Getting image: " <> url
-    asset_host = Application.get_env(:arc, :asset_host)
-    cond do
-        url =~ asset_host -> url
-        true ->
-            url
-            |> get_remote_image
-            |> store_image
-            |> get_cached_image_url
-    end
-end
+  def cache_remote_image(url) do
+      IO.puts "Getting image: " <> url
+      asset_host = Application.get_env(:arc, :asset_host)
+      cond do
+          url =~ asset_host -> url
+          true ->
+              url
+              |> get_remote_image
+              |> store_image
+              |> get_cached_image_url
+      end
+  end
 
-def get_remote_image(url) do
-    url = cond do
-      String.starts_with?(url, "//") -> "https:" <> url
-      true -> url
-    end
-    uuid = Ecto.UUID.generate()
-    %HTTPoison.Response{headers: headers, body: body} = HTTPoison.get!(url, [], [follow_redirect: true])
-    content_type = get_header(headers, "Content-Type")
-    ext = Plug.MIME.extensions(content_type || "image/jpeg") |> hd
-    path = Plug.Upload.random_file!("image")
-    File.write!(path, body)
-    upload = %Plug.Upload{content_type: content_type, filename: "#{uuid}.#{ext}", path: path}
-    IO.puts "Generated upload: "
-    IO.inspect upload
-    upload
-end
+  def get_remote_image(url) do
+      url = cond do
+        String.starts_with?(url, "//") -> "https:" <> url
+        true -> url
+      end
+      uuid = Ecto.UUID.generate()
+      %HTTPoison.Response{headers: headers, body: body} = HTTPoison.get!(url, [], [follow_redirect: true])
+      content_type = get_header(headers, "Content-Type")
+      ext = Plug.MIME.extensions(content_type || "image/jpeg") |> hd
+      path = Plug.Upload.random_file!("image")
+      File.write!(path, body)
+      upload = %Plug.Upload{content_type: content_type, filename: "#{uuid}.#{ext}", path: path}
+      IO.puts "Generated upload: "
+      IO.inspect upload
+      upload
+  end
 
-def store_image(upload) do
-    {:ok, filename} = store(upload)
-    filename
-end
+  def store_image(upload) do
+      {:ok, filename} = store(upload)
+      filename
+  end
 
-def get_cached_image_url(filename) do
-    url(filename)
-end
-
-
-defp get_header(headers, key) do
-    headers
-    |> Enum.filter(fn({k, _}) -> k == key end)
-    |> hd
-    |> elem(1)
-end
+  def get_cached_image_url(filename) do
+      url(filename)
+  end
 
 
-def replace_images(content) do
-    Regex.scan(
-      ~r/<img.*?src="(.*?)"/s, 
-      content, 
-      capture: :all_but_first
-      ) 
-    |> Enum.reduce([], fn (x, acc) -> acc ++ [x |> hd] end)
-    |> Enum.reduce([], fn (x, acc) -> acc ++ [[x, cache_remote_image(x)]] end)
-    |> Enum.reduce(content, fn(x, acc) -> String.replace(content, List.first(x), List.last(x)) end)
-end
+  defp get_header(headers, key) do
+      headers
+      |> Enum.filter(fn({k, _}) -> k == key end)
+      |> hd
+      |> elem(1)
+  end
+
+
+  def replace_images(content) do
+      Regex.scan(
+        ~r/<img.*?src="(.*?)"/s, 
+        content, 
+        capture: :all_but_first
+        ) 
+      |> Enum.reduce([], fn (x, acc) -> acc ++ [x |> hd] end)
+      |> Enum.reduce([], fn (x, acc) -> acc ++ [[x, cache_remote_image(x)]] end)
+      |> Enum.reduce(content, fn(x, acc) -> String.replace(content, List.first(x), List.last(x)) end)
+  end
 
 
 end
